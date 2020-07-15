e99080cdf9b4cfd1a631d71d6921cd9932d0f7a5
Nick <nmcarducci@gmail.com>
2020-07-15 04:49:04 -0400

    ok

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Switch } from "react-router-dom";
+import "./App.css";
+
+import React from "react";
+const App = () => {
+  return (
+    <>
+      <Switch>
+      </Switch>
+    </>
+  );
+};
+
+export default App;
